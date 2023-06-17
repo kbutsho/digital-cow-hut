17c5871b2e4b842052c748f830b758796a20bd15
Kaushik Biswas <kbutsho@gmail.com>
2023-06-17 23:36:29 +0600

    update

---
+++ build/app/modules/user/user.model.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['seller', 'buyer'],
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    budget: {
+        type: Number
+    },
+    income: {
+        type: Number
+    },
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+    },
+});
+exports.User = (0, mongoose_1.model)('User', userSchema);
---
+++ build/app/modules/user/user.service.js
@@ -0,0 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserService = void 0;
+const user_model_1 = require("./user.model");
+const create = (data) => __awaiter(void 0, void 0, void 0, function* () {
+    const newData = Object.assign(Object.assign({}, data), { income: 0 });
+    const result = yield user_model_1.User.create(newData);
+    return result;
+});
+const getAllUser = () => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield user_model_1.User.find();
+    return result;
+});
+const getSingleUser = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield user_model_1.User.findById(id);
+    return result;
+});
+const update = (id, data) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield user_model_1.User.findOneAndUpdate({ _id: id }, data, { new: true });
+    return result;
+});
+const deleteUser = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield user_model_1.User.findByIdAndDelete(id);
+    return result;
+});
+exports.UserService = {
+    create,
+    getAllUser,
+    getSingleUser,
+    update,
+    deleteUser
+};
